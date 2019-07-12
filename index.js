f79067edfab54b3ff1f558e3a10ed3261955c622
Carbon Alabel <carbonalabel@gmail.com>
2019-07-12 05:47:59 +0200

    Refactor almost everything

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addBadge(name, url) {
+function addBadge(url, name = "") {
     const badge = document.createElement("div");
     badge.innerHTML = `<img src="${url}"><br/>${name}<br/>
 <select onchange="this.parentElement.children[0].className = this.value + '-border'">
@@ -13,38 +13,37 @@ function addBadge(name, url) {
     document.getElementById("portraits").appendChild(badge);
 }
 
-async function addPortrait() {
-    let input = document.getElementById("char-name").value;
-    document.getElementById("add-char-form").reset();
-    let request = fetch("https://esi.evetech.net/v1/universe/ids/", {method: "POST", body: JSON.stringify([input])});
+async function addPortrait({character}) {
+    let request = fetch("https://esi.evetech.net/v1/universe/ids/", {method: "POST", body: JSON.stringify([character])});
     let response = await request.then(response => response.json());
     if (!response.characters) {
-        throw new Error(`Couldn't find character with name "${input}"`);
+        throw new Error(`Couldn't find character with name "${character}"`);
     }
-    let {id, name} = response.characters.find(character => character.name.toLowerCase() === input.trim().toLowerCase());
-    addBadge(name, `https://imageserver.eveonline.com/Character/${id}_1024.jpg`);
+    let {id, name} = response.characters.find(match => match.name.toLowerCase() === character.toLowerCase());
+    addBadge(`https://imageserver.eveonline.com/Character/${id}_1024.jpg`, name);
 }
 
-function addImage() {
-    let input = document.getElementById("image-url").value;
-    document.getElementById("add-image-form").reset();
-    addBadge("", input);
+function addImage({url}) {
+    addBadge(url);
 }
 
 function removeAll() {
     const portraits = document.getElementById("portraits");
-    for (let e of portraits.querySelectorAll("div")) {
+    for (let e of Array.from(portraits.children)) {
         portraits.removeChild(e);
     }
 }
 
+function onFormSubmit(formName, submitFunction) {
+    const form = document.forms[formName];
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        submitFunction(Object.fromEntries(new FormData(form)));
+        form.reset();
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("add-char-form").addEventListener("submit", event => {
-        event.preventDefault();
-        addPortrait().catch(error => alert(error.message));
-    });
-    document.getElementById("add-image-form").addEventListener("submit", event => {
-        event.preventDefault();
-        addImage();
-    });
+    onFormSubmit("add-char-form", addPortrait);
+    onFormSubmit("add-image-form", addImage);
 });
