09267da2df89cd62f2212f64f6a9e5df8ceba05b
Carbon Alabel <carbonalabel@gmail.com>
2019-07-26 05:34:37 +0200

    Add search function

--- index.js
+++ index.js
@@ -17,6 +17,53 @@ function addBadge(url, name = "", bgSelector = false) {
     document.getElementById("badges").appendChild(badge);
 }
 
+function addEntity({id, name, category}) {
+    switch (category) {
+        case "character":
+        case "inventory_type":
+            addBadge(entityImageURL({id, category}), name);
+            break;
+        case "corporation":
+        case "alliance":
+        case "faction":
+            addBadge(entityImageURL({id, category}), name, true);
+            break;
+        default:
+            throw new Error(`Invalid category "${category}"`);
+    }
+}
+
+function entityImageURL({id, category}) {
+    switch (category) {
+        case "character":
+            return `https://imageserver.eveonline.com/Character/${id}_1024.jpg`
+        case "corporation":
+            return `https://imageserver.eveonline.com/Corporation/${id}_256.png`;
+        case "alliance":
+        case "faction":
+            return `https://imageserver.eveonline.com/Alliance/${id}_128.png`;
+        case "inventory_type":
+            return `https://imageserver.eveonline.com/Render/${id}_512.png`;
+        default:
+            throw new Error(`Invalid category "${category}"`);
+    }
+}
+
+function addSearchResult({id, name, category}) {
+    let searchResult = document.createElement("div");
+    searchResult.style.order = {
+        "inventory_type": 1,
+        "faction": 2,
+        "alliance": 3,
+        "corporation": 4,
+        "character": 5
+    }[category];
+    searchResult.innerHTML = `<img src="${entityImageURL({id, category})}">
+<span>${name} (${category})</span>
+<button onclick="addEntity({id:${id},name:'${name}',category:'${category}'})">Add</button>`;
+    document.getElementById("search-results").appendChild(searchResult);
+}
+
 async function request(...args) {
     let request = await fetch(...args);
     if (!request.ok) {
@@ -32,6 +79,26 @@ async function addPortrait({character}) {
     }
     let {id, name} = response.characters.find(match => match.name.toLowerCase() === character.toLowerCase());
     addBadge(`https://imageserver.eveonline.com/Character/${id}_1024.jpg`, name);
+}
+
+async function search({query}) {
+    let searchResults = document.getElementById("search-results");
+    Array.from(searchResults.children).forEach(element => element.remove());
+    let search = await request(`https://esi.evetech.net/v2/search/?categories=alliance,character,corporation,faction,inventory_type&search=${encodeURIComponent(query)}`);
+    let ids = [];
+    for (let category in search) {
+        if (Array.isArray(search[category])) {
+            ids.push(...search[category]);
+        }
+    }
+    let names = await request("https://esi.evetech.net/v3/universe/names/", {method: "POST", body: JSON.stringify(ids)});
+    if (!Array.isArray(names) || !names.length) {
+        throw new Error(`No search results for "${query}"`);
+    }
+    let header = document.createElement("header");
+    header.textContent = `Search results for "${query}":`;
+    searchResults.appendChild(header);
+    names.forEach(addSearchResult);
 }
 
 function addImage({url, name}) {
@@ -64,4 +131,5 @@ function onFormSubmit(formName, submitFunction) {
 document.addEventListener("DOMContentLoaded", () => {
     onFormSubmit("add-char-form", addPortrait);
     onFormSubmit("add-image-form", addImage);
+    onFormSubmit("search-form", search);
 });
