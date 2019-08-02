4a92ca4ec940d564e791369846b33951326b2582
Carbon Alabel <carbonalabel@gmail.com>
2019-08-02 12:32:51 +0200

    Add comments to the JS code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Add a badge to the badge area, using the given image URL and name, with an optional background color toggle.
 function addBadge(url, name = "", bgSelector = false) {
     let badge = document.createElement("div");
     badge.innerHTML = `<img src="${url}"><br>${name}<br>
@@ -17,15 +18,18 @@ function addBadge(url, name = "", bgSelector = false) {
     document.getElementById("badges").appendChild(badge);
 }
 
+// Add a badge for an EVE entity.
 function addEntity({id, name, category}) {
     switch (category) {
         case "character":
         case "inventory_type":
+            // Character portraits and type renders are solid images, the background color selector would serve no purpose.
             addBadge(entityImageURL({id, category}), name);
             break;
         case "corporation":
         case "alliance":
         case "faction":
+            // Logos might have transparency in them, the background color selector should be displayed.
             addBadge(entityImageURL({id, category}), name, true);
             break;
         default:
@@ -33,6 +37,7 @@ function addEntity({id, name, category}) {
     }
 }
 
+// Return the URL of a logo/render for an EVE entity, in the highest resolution available.
 function entityImageURL({id, category}) {
     switch (category) {
         case "character":
@@ -49,8 +54,10 @@ function entityImageURL({id, category}) {
     }
 }
 
+// Add a search result card to the search result area.
 function addSearchResult({id, name, category}) {
     let searchResult = document.createElement("div");
+    // Some entity types should be shown above others.
     searchResult.style.order = {
         "inventory_type": 2,
         "faction": 1,
@@ -64,6 +71,7 @@ function addSearchResult({id, name, category}) {
     document.getElementById("search-results").appendChild(searchResult);
 }
 
+// Make a request to ESI, throwing an error if it fails, or returning the parsed JSON response.
 async function request(...args) {
     let request = await fetch(...args);
     if (!request.ok) {
@@ -72,10 +80,12 @@ async function request(...args) {
     return request.json();
 }
 
+// Empty an element of any children.
 function removeAllChildren(parentElement) {
     Array.from(parentElement.children).forEach(element => element.remove());
 }
 
+// Add a badge for a character with the exact name entered.
 async function addPortrait({character}) {
     let response = await request("https://esi.evetech.net/v1/universe/ids/", {method: "POST", body: JSON.stringify([character])});
     if (!response.characters) {
@@ -85,49 +95,64 @@ async function addPortrait({character}) {
     addEntity({id, name, category: "character"});
 }
 
+// Perform a search.
 async function search({query}) {
     let searchResults = document.getElementById("search-results");
+    // Clear the search result area.
     removeAllChildren(searchResults);
     let search = await request(`https://esi.evetech.net/v2/search/?categories=alliance,character,corporation,faction,inventory_type&search=${encodeURIComponent(query)}`);
+    // Merge all the search result categories into a single array of IDs.
     let ids = [];
     for (let category in search) {
         if (Array.isArray(search[category])) {
             ids.push(...search[category]);
         }
     }
+    // Send the ID list to the names endpoint, to get an array with IDs, names, and categories for each result.
     let names = await request("https://esi.evetech.net/v3/universe/names/", {method: "POST", body: JSON.stringify(ids)});
     if (!Array.isArray(names) || !names.length) {
         throw new Error(`No search results for "${query}"`);
     }
+    // Add the search result header.
     let header = document.createElement("header");
     header.textContent = `Search results for "${query}":`;
     searchResults.appendChild(header);
+    // Add search result cards for each of the results.
     names.forEach(addSearchResult);
 }
 
+// Add a badge for the entered image URL and name.
 function addImage({url, name}) {
     addBadge(url, name, true);
 }
 
+// Remove all badges from the badge area, asking the user for confirmation first.
 function removeAll() {
     if (confirm("Are you sure you want to remove all badges?")) {
         removeAllChildren(document.getElementById("badges"));
     }
 }
 
+// Common form submission handling.
 function onFormSubmit(formName, submitFunction) {
     let form = document.forms[formName];
     form.addEventListener("submit", event => {
+        // Prevent form submission.
         event.preventDefault();
+        // Convert the form data to an object.
         let formData = {};
         for (let [key, value] of new FormData(form)) {
             formData[key] = value.trim();
         }
+        // Run the associated function, passing it the form data.
+        // Use the browser alert function to handle any errors.
         Promise.resolve(submitFunction(formData)).catch(alert);
+        // Clear the form.
         form.reset();
     });
 }
 
+// Register all the form actions.
 document.addEventListener("DOMContentLoaded", () => {
     onFormSubmit("add-char-form", addPortrait);
     onFormSubmit("add-image-form", addImage);
